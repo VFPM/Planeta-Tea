0db772e25182380e8350810ba61adcd1d6392715
Chobbi <victor.perezml@uanl.edu.mx>
2022-07-12 09:13:12 -0500

    cambios a web

--- api/resources/views/system/Response/btn.blade.php
+++ api/resources/views/system/Response/btn.blade.php
@@ -1,18 +1,18 @@
 <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 
-<a href="#" class="btn-primary" data-toggle="modal" data-target="#modal-info-{{$id}}">Ver Información</a>
+<a href="#" data-toggle="modal" data-target="#modal-info-{{$id}}">Ver Información</a>
 
 <!-- Modal Crear Tipo de Noticia -->
 <div class="modal fade" id="modal-info-{{$id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-        <div class="modal-content">
+        <div class="modal-content print">
             <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLongTitle">Información de la respuesta</h5>
             <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button> -->
             </div>
-            <div class="modal-body">
+            <div class="modal-body print">
             <div class="mb-3 col-sm-12 col-mb-12 col-xl-12">
                     <!-- <table id="data2" class="dataindex table table-flush"> 
                         <thead class="thead-light">
@@ -40,11 +40,11 @@
                     </table> -->
                     
                     <table id="data{{$id}}" class="dataindex table table-flush w-100 table-striped">
-                        <thead class="thead-light ">
+                        <thead class="thead-light">
                         <tr >
                             <th >Pregunta</th>
                             <th >Respuesta</th>
-                            <th >Respuesta2</th>
+                            <th >Descripción</th>
                         </tr>
                         </thead>
                     </table>
@@ -52,14 +52,15 @@
                 </div>
                     
             </div>
+           
             <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal" onClick="window.print()">Imprimir</button>
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
             </div>
         </div>
     </div>
-</div>
 
-    
+</div>
 
     <script>
          $(document).ready(function () {
